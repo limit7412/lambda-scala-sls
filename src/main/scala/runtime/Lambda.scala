162697ff858cc9e279b892bb745705992cd3b9d2
qazx7412 <dev@newgenerations.im>
2021-06-20 13:54:23 +0900

    change use recursion

--- src/main/scala/runtime/Lambda.scala
+++ src/main/scala/runtime/Lambda.scala
@@ -16,46 +16,48 @@ object Lambda {
   )
   @JsonCodec case class Response(statusCode: Int, body: String)
 
-  def Handler[T](
+  def Handler(
       name: String,
       callback: Request => Response
   ): Lambda.type = {
-    if (name != sys.env("_HANDLER")) {
-      return this
+    if (name == sys.env("_HANDLER")) {
+      handler(callback)
     }
 
-    while (true) {
-      var response =
-        Http.Get(
-          s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/next"
-        )
-      val requestID =
-        response.headers().firstValue("Lambda-Runtime-Aws-Request-Id").get()
+    this
+  }
+  private def handler(callback: Request => Response): Lambda.type = {
+    var response =
+      Http.Get(
+        s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/next"
+      )
+    val requestID =
+      response.headers().firstValue("Lambda-Runtime-Aws-Request-Id").get()
 
-      decode[Request](response.body()) match {
-        case Right(event: Request) => {
-          try {
-            val result = callback(event)
+    decode[Request](response.body()) match {
+      case Right(event: Request) => {
+        try {
+          val result = callback(event)
+          Http.Post(
+            s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/next",
+            result.asJson.noSpaces
+          )
+        } catch {
+          case e: Exception => {
             Http.Post(
-              s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/next",
-              result.asJson.noSpaces
+              s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/error",
+              Response(
+                500,
+                "{\"msg\", \"Internal Lambda Error\"}"
+              ).asJson.noSpaces
             )
-          } catch {
-            case e: Exception => {
-              Http.Post(
-                s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/error",
-                Response(
-                  500,
-                  "{\"msg\", \"Internal Lambda Error\"}"
-                ).asJson.noSpaces
-              )
-            }
           }
         }
-        case Left(_) => throw new Exception("failed to decode request event")
       }
+      case Left(_) => throw new Exception("failed to decode request event")
     }
 
+    // handler(callback)
     this
   }
 }
