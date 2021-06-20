a2f52e6dad3653ce797da7d321426c34bf9c7069
qazx7412 <dev@newgenerations.im>
2021-06-20 16:18:39 +0900

    debug

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -21,8 +21,9 @@ object Main {
       .Handler(
         "world",
         (event) => {
-          println(event.headers)
-          println(event.body)
+          println(event)
+          // println(event.headers)
+          // println(event.body)
           serverless.Lambda.Response(
             200,
             SampleResponse("でしょうねミスター・サーバーレス").asJson.noSpaces
--- src/main/scala/runtime/Lambda.scala
+++ src/main/scala/runtime/Lambda.scala
@@ -18,14 +18,14 @@ object Lambda {
   @JsonCodec case class Response(statusCode: Int, body: String)
   @JsonCodec case class ErrorResponse(msg: String, error: String)
 
-  def Handler(name: String, callback: Request => Response): Lambda.type = {
+  def Handler(name: String, callback: String => Response): Lambda.type = {
     if (name == sys.env("_HANDLER")) {
       handler(callback)
     }
 
     this
   }
-  private def handler(callback: Request => Response): Lambda.type = {
+  private def handler(callback: String => Response): Lambda.type = {
     var response =
       Http.Get(
         s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/next"
@@ -33,32 +33,33 @@ object Lambda {
     val requestID =
       response.headers().firstValue("Lambda-Runtime-Aws-Request-Id").get()
 
-    decode[Request](response.body()) match {
-      case Right(event: Request) => {
-        try {
-          val result = callback(event)
-          Http.Post(
-            s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/response",
-            result.asJson.noSpaces
-          )
-        } catch {
-          case e: Exception => {
-            e.printStackTrace()
-            Http.Post(
-              s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/error",
-              Response(
-                500,
-                ErrorResponse(
-                  "Internal Lambda Error",
-                  e.getMessage()
-                ).asJson.noSpaces
-              ).asJson.noSpaces
-            )
-          }
-        }
+    val event = response.body()
+    // decode[Request](response.body()) match {
+    //   case Right(event: Request) => {
+    try {
+      val result = callback(event)
+      Http.Post(
+        s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/response",
+        result.asJson.noSpaces
+      )
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
+        Http.Post(
+          s"http://${sys.env("AWS_LAMBDA_RUNTIME_API")}/2018-06-01/runtime/invocation/$requestID/error",
+          Response(
+            500,
+            ErrorResponse(
+              "Internal Lambda Error",
+              e.getMessage()
+            ).asJson.noSpaces
+          ).asJson.noSpaces
+        )
       }
-      case Left(_) => throw new Exception("failed to decode request event")
     }
+    //   }
+    //   case Left(_) => throw new Exception("failed to decode request event")
+    // }
 
     handler(callback)
     this
