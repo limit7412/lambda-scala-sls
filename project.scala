873bdaf1cf6d96d30c6429b8ba47009054309848
kairox <git@oxymoron.link>
2026-04-08 20:43:20 +0900

    lib verup

--- project.scala
+++ project.scala
@@ -5,5 +5,5 @@
 //> using platform native
 //> using nativeTarget library-static
 //> using toolkit default
-//> using dep "com.softwaremill.sttp.client4::core:4.0.0-M14"
-//> using dep "com.lihaoyi::upickle:3.3.1"
+//> using dep "com.softwaremill.sttp.client4::core:4.0.21"
+//> using dep "com.lihaoyi::upickle:4.4.3"
